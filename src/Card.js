30473eb2d47320a192d817f548ca93c10fda9fea
Vaibhavksanghani <vaibhavsanghani35@gmail.com>
2024-02-04 11:52:47 +0530

    creating robofriends

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+        <img alt='robots' src='https://robohash.org/generate?200x200'/>
+        <div>
+            <h2>John Cena</h2>
+            <p>johncenaaa@gmail.com</p>
+        </div>
+    </div>
+  )
+}
+
+export default Card
